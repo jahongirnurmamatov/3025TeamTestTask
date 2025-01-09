17e442ad819c919f0aea9d473b39aae0094606ca
jahongiir.nurmamatov <zokir.almatov1414@gmail.com>
2025-01-09 13:56:34 +0500

    expiresAt functionality added

--- server/controllers/shortenUrl.controller.js
+++ server/controllers/shortenUrl.controller.js
@@ -3,7 +3,7 @@ import ShortUrl from "../models/shortUrl.model.js";
 
 export const shortenUrl = async (req, res) => {
   try {
-    const { longUrl } = req.body;
+    const { longUrl, expiresIn } = req.body;
 
     // check if exists already
     const exists = await ShortUrl.findOne({ longUrl });
@@ -16,7 +16,12 @@ export const shortenUrl = async (req, res) => {
       shortUrl = generateHash256(longUrl).slice(0, 6);
     } while (await ShortUrl.findOne({ shortUrl }));
 
-    const newShortUrl = await ShortUrl.create({ longUrl, shortUrl });
+    let expiresAt = null;
+    if (expiresIn) {
+        expiresAt = new Date();
+        expiresAt.setUTCHours(expiresAt.getUTCHours() + expiresIn); 
+    }
+    const newShortUrl = await ShortUrl.create({ longUrl, shortUrl, expiresAt });
     await newShortUrl.save();
     res.status(200).json({ shortUrl: newShortUrl.shortUrl });
   } catch (error) {
@@ -29,6 +34,11 @@ export const getShortUrlAndRedirect = async (req, res) => {
     const { shortUrl } = req.params;
     const url = await ShortUrl.findOne({ shortUrl });
     if (!url) return res.status(404).json({ message: "Not Found" });
+
+    if (url.expiresAt && new Date() > url.expiresAt) {
+        return res.status(410).json({ error: "URL Gone" });  
+    }
+
     if (url) {
       url.clicks++;
       await url.save();
@@ -45,13 +55,11 @@ export const getShortUrlInfo = async (req, res) => {
     const url = await ShortUrl.findOne({ shortUrl });
     if (!url) return res.status(404).json({ message: "Not Found" });
     if (url) {
-      return res
-        .status(200)
-        .json({
-          longUrl: url.longUrl,
-          createdAt: url.createdAt,
-          clicks: url.clicks,
-        });
+      return res.status(200).json({
+        longUrl: url.longUrl,
+        createdAt: url.createdAt,
+        clicks: url.clicks,
+      });
     }
   } catch (error) {}
 };
