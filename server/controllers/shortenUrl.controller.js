84ad5b47df36bc3804305d0927c2c31dde72783b
jahongiir.nurmamatov <zokir.almatov1414@gmail.com>
2025-01-09 17:47:38 +0500

    optional alias and ip tracker

--- server/controllers/shortenUrl.controller.js
+++ server/controllers/shortenUrl.controller.js
@@ -4,23 +4,36 @@ import Visit from "../models/visit.model.js";
 
 export const shortenUrl = async (req, res) => {
   try {
-    const { longUrl, expiresIn } = req.body;
+    const { longUrl, expiresIn, alias } = req.body;
 
+    if (alias && alias.length > 20) {
+      return res
+        .status(400)
+        .json({ error: "Alias must be up to 20 characters long." });
+    }
     // check if exists already
+    let shortUrl = alias;
+    if (!alias) {
+      do {
+        shortUrl = generateHash256(longUrl).slice(0, 6);
+      } while (await ShortUrl.findOne({ shortUrl }));
+    }
+    const aliasExists = await ShortUrl.findOne({ shortUrl });
+    if (aliasExists) {
+      return res.status(400).json({ error: "Alias already exists." });
+    }
+
+    // Check if the original URL already has a short URL
     const exists = await ShortUrl.findOne({ longUrl });
     if (exists) {
       return res.status(200).json({ shortUrl: exists.shortUrl });
     }
     //  this is just to awoid possible collisions even if chance is small
-    let shortUrl;
-    do {
-      shortUrl = generateHash256(longUrl).slice(0, 6);
-    } while (await ShortUrl.findOne({ shortUrl }));
 
     let expiresAt = null;
     if (expiresIn) {
-        expiresAt = new Date();
-        expiresAt.setUTCHours(expiresAt.getUTCHours() + expiresIn); 
+      expiresAt = new Date();
+      expiresAt.setUTCHours(expiresAt.getUTCHours() + expiresIn);
     }
     const newShortUrl = await ShortUrl.create({ longUrl, shortUrl, expiresAt });
     await newShortUrl.save();
@@ -37,21 +50,21 @@ export const getShortUrlAndRedirect = async (req, res) => {
     if (!url) return res.status(404).json({ message: "Not Found" });
 
     if (url.expiresAt && new Date() > url.expiresAt) {
-        return res.status(410).json({ error: "URL Gone" });  
+      return res.status(410).json({ error: "URL Gone" });
     }
 
     if (url) {
       url.clicks++;
       await url.save();
     }
-    
-    const ip = req.ip || req.headers['x-forwarded-for']?.split(',')[0]; 
+
+    const ip = req.ip || req.headers["x-forwarded-for"]?.split(",")[0];
 
     await Visit.create({
-      shortUrl: url._id,  
+      shortUrl: url._id,
       ip: ip,
     });
-    
+
     return res.status(200).json({ longUrl: url.longUrl });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -87,22 +100,23 @@ export const deleteShortUrl = async (req, res) => {
   }
 };
 
-
 export const getAnalytics = async (req, res) => {
-    const { shortUrl } = req.params;
-    
-    try {
-      const url = await ShortUrl.findOne({ shortUrl });
-      
-      if (!url) {
-        return res.status(404).json({ error: "Short URL not found" });
-      }
-      
-      const visits = await Visit.find({ shortUrl: url._id }).select("ip visitedAt -_id").sort({ visitedAt: -1 }).limit(5);  
-      
-      res.status(200).json({ clicks: url.clicks, lastVisits: visits });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+  const { shortUrl } = req.params;
+
+  try {
+    const url = await ShortUrl.findOne({ shortUrl });
+
+    if (!url) {
+      return res.status(404).json({ error: "Short URL not found" });
     }
-  };
-  
+
+    const visits = await Visit.find({ shortUrl: url._id })
+      .select("ip visitedAt -_id")
+      .sort({ visitedAt: -1 })
+      .limit(5);
+
+    res.status(200).json({ clicks: url.clicks, lastVisits: visits });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
